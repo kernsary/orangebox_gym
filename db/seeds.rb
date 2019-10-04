7b154269889958189688205677480236008f9133
Charles Kennedy <charlesscottkennedy@googlemail.com>
2019-10-04 22:58:05 +0100

    Member class finished and methods tested

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,13 @@
 require_relative("../models/member")
 require("pry")
 
+Member.delete_all
+
 member1 = Member.new({"first_name" => "John", "last_name" => "Smith", "date_of_birth" => "5/10/1995"})
+member2 = Member.new({"first_name" => "Sarah", "last_name" => "Docherty", "date_of_birth" => "4/9/1994"})
 
 member1.save()
+member2.save()
 
 binding.pry
 nil
--- models/member.rb
+++ models/member.rb
@@ -2,7 +2,8 @@ require_relative("../db/sql_runner")
 
 class Member
 
-  attr_reader :id, :first_name, :last_name, :date_of_birth
+  attr_reader :id
+  attr_accessor :first_name, :last_name, :date_of_birth
 
   def initialize(options)
     @id = options["id"].to_i() if options["id"]
@@ -18,6 +19,44 @@ class Member
     RETURNING id"
     values = [@first_name, @last_name, @date_of_birth]
     @id = SqlRunner.run(sql, values)[0]["id"].to_i()
+  end
+
+  def update()
+    sql = "UPDATE members
+    SET (first_name, last_name, date_of_birth) = ($1, $2, $3)
+    WHERE id = $4"
+    values = [@first_name, @last_name, @date_of_birth, @id]
+    SqlRunner.run(sql, values)
+  end
+
+  def delete()
+    sql = "DELETE FROM members WHERE id = $1"
+    values = [@id]
+    SqlRunner.run(sql, values)
+  end
+
+  def self.all()
+    sql = "SELECT * FROM members"
+    result = SqlRunner.run(sql)
+    return self.map_members(result)
+  end
+
+  def self.find(id)
+    sql = "SELECT * FROM members
+    WHERE id = $1"
+    values = [id]
+    result = SqlRunner.run(sql, values)[0]
+    return Member.new(result)
+  end
+
+  def self.delete_all()
+    sql = "DELETE FROM MEMBERS"
+    SqlRunner.run(sql)
+  end
+
+  def self.map_members(data)
+    result = data.map{|member| Member.new(member)}
+    return result
   end
 
 end
