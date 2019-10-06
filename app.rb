a9c12f66c933f41123873dc3dcc8b09c064bad11
Charles Kennedy <charlesscottkennedy@googlemail.com>
2019-10-06 14:00:34 +0100

    app.rb and layout.erb created and tested

---
+++ app.rb
@@ -0,0 +1,9 @@
+require("sinatra")
+require("sinatra/contrib/all")
+# require_relative("controllers/activities_controller")
+# require_relative("controllers/members_controller")
+# require_relative("contollers/bookings_controller")
+
+get "/gym" do
+  erb(:home)
+end
