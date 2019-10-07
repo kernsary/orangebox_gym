d451895fbc34d1ba51dc7924e120fca0b411b13c
Charles Kennedy <charlesscottkennedy@googlemail.com>
2019-10-07 10:39:17 +0100

    all methods and views created in members_controller

--- controllers/members_controller.rb
+++ controllers/members_controller.rb
@@ -12,10 +12,27 @@ end
 
 post "/gym/members" do
   Member.new(params).save()
-  redirect to "gym/members"
+  redirect to "/gym/members"
 end
 
 get "/gym/members/:id" do
 @member = Member.find(params["id"])
   erb(:"members/show")
+end
+
+get "/gym/members/:id/edit" do
+  @member = Member.find(params["id"])
+  erb(:"members/edit")
+end
+
+post "/gym/members/:id" do
+  member = Member.new(params)
+  member.update()
+  redirect to "/gym/members/#{params["id"]}"
+end
+
+post "/gym/members/:id/delete" do
+  member = Member.find(params["id"])
+  member.delete()
+  redirect to "/gym/members"
 end
